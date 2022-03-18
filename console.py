3d8c4d572cb131fc6c06864b680e5b0f3f67cfe4
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-18 12:25:15 +0000

    parent_repo completed

--- console.py
+++ console.py
@@ -1,16 +1,24 @@
 import pdb
 from models.doctor import Doctor
+from models.parent import Parent
 
 import repos.doctor_repo as doctor_repo
+import repos.parent_repo as parent_repo
 
 doctor1=Doctor('Bob', "097-131455", "bob@hotmail.com", "5 vet road EH2 9TR")
 doctor2=Doctor("John", "098-126327", "John@hotmail.com", "6 vet road EH2 9TR")
 
-doctor_repo.delete(2)
+parent1=Parent("Clark Kent", "098-384738", "superman@hotmail.com", "Krypton")
+parent2=Parent("Bruce Wayne", "736-318476", "batman@batcomputer.gov", "Wayne manor")
 
-doctors = doctor_repo.select_all()
-for doctor in doctors:
-    print(doctor.__dict__)
+parent_repo.save(parent1)
+parent_repo.save(parent2)
+
+
+
+parents = parent_repo.select_all()
+for parent in parents:
+    print(parent.__dict__)
 
 
 
---
+++ repos/parent_repo.py
@@ -0,0 +1,36 @@
+from db.run_sql import run_sql
+
+from models.parent import Parent
+
+def select_all():
+    parents = []
+    sql = "SELECT * FROM parents"
+    results = run_sql(sql)
+    
+    for row in results:
+        parent = Parent(row['name'], row['phone'], row['email'], row['address'], row['id'])
+        parents.append(parent)
+    return parents
+
+def save(parent):
+    sql= "INSERT INTO parents (name, phone, email, address) VALUES (%s, %s, %s, %s) RETURNING id"
+    values = [parent.name, parent.phone, parent.email, parent.address]
+    result = run_sql(sql, values)
+    parent.id = result[0]['id']
+    return parent
+
+def delete_all():
+    sql = "DELETE FROM parents"
+    run_sql(sql)
+
+def select(id):
+    sql = "SELECT * FROM parents WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    parent = Parent(result['name'], result['phone'], result['email'], result['address'], result['id'])
+    return parent
+
+def delete(id):
+    sql = "DELETE FROM parents WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
