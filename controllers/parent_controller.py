507fdc358dcf0f7606b205752a17171996a1af0d
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-18 18:29:29 +0000

    finished individual show for parents

--- controllers/parent_controller.py
+++ controllers/parent_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from models.parent  import Parent
 
 import repos.parent_repo as parent_repo
 import repos.pet_repo as pet_repo
@@ -11,3 +12,17 @@ def parents():
     pets = pet_repo.select_all()
     parents=parent_repo.select_all()
     return render_template('/parents/index.html', parents=parents, pets=pets)
+
+@parents_blueprint.route('/parents/new')
+def new_parent():
+    return render_template('/parents/new_parent.html')
+
+@parents_blueprint.route('/parents/new', methods=['POST'])
+def add_parent():
+    parent_repo.save(Parent(request.form['name'], request.form['phone'], request.form['email'], request.form['address']))
+    return redirect('/parents')
+
+@parents_blueprint.route('/parents/show/<id>')
+def show_parent(id):
+    parent = parent_repo.select(id)
+    return render_template('/parents/show.html', parent=parent)
